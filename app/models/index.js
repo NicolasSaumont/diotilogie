eb01e0c9d4409c3d85c1e6886af9639ae90f399b
Nicolas Saumont <saumont.nicolas@gmail.com>
2023-08-24 16:06:29 +0200

    feat models/index.js

---
+++ app/models/index.js
@@ -0,0 +1,33 @@
+const User = require('./user');
+const Question = require('./question');
+const Tag = require('./tag');
+
+//! Association user - question
+// Un user peut créer plusieurs questions
+User.hasMany(Question, {
+    as: 'questionsFromUser',
+    foreignKey: 'user_id'
+});
+
+// Une question est créée par un seul user
+Question.belongsTo(User, {
+    as: 'userOfQuestion',
+    foreignKey: 'user_id'
+});
+
+//! Association tag - question
+// Une question peut appartenir a plusieurs tags
+Question.belongsToMany(Tag, {
+    through: 'question_has_tag',
+    foreignKey: 'question_id',
+    otherKey: 'tag_id',
+    as: 'tagsFromQuestion'
+})
+
+// Un tag peut être appartenu par plusieurs questions
+Tag.belongsToMany(Question, {
+    through: 'question_has_tag',
+    foreignKey: 'tag_id',
+    otherKey: 'question_id',
+    as: 'questionsFromTags'
+})
